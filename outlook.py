bbd0399dec78697da303d8393994c9d6944cd8e5
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2022-03-22 13:12:32 +0000

    export tokens to csv

--- outlook.py
+++ outlook.py
@@ -1,14 +1,23 @@
 import win32com.client
- 
+import csv
+import numpy
+
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
 inbox = outlook.GetDefaultFolder(6)
 messages = inbox.Items
 
+tokens = []
 for message in messages:
     if message.Subject =='Workspace ONE UEM Device Activation':
         date = message.ReceivedTime.strftime("%d/%m/%Y")
         if(date == '22/03/2022'): 
             token_index = message.body.index('Token: ')
             token = message.body[token_index+7:token_index+13]
-            print(token)
+            tokens.append(token)
+
+
+resultFyle = open("output.csv",'w')
+for r in tokens:
+    resultFyle.write(r + "\n")
+resultFyle.close()
